c9a29e79f54073eac7f16074721eaaf1ab64df2a
iamtun <letuan19431791.iuh@gmail.com>
2024-02-06 16:53:36 +0700

    feat: enable cors

--- backend-app/index.js
+++ backend-app/index.js
@@ -1,14 +1,21 @@
 ﻿import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors({
+  origin: '*'
+}))
+
 const PORT = process.env.PORT;
 
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello, World!' });
 });
+
 
 
 app.listen(PORT, () => {
