7ae7bba71d17a2549f22c0b6857f33a9e3f6ec07
iamtun <letuan19431791.iuh@gmail.com>
2024-02-06 16:09:39 +0700

    feat: init simple api

--- backend-app/index.js
+++ backend-app/index.js
@@ -1,1 +1,16 @@
-﻿
+﻿import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello, World!' });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
